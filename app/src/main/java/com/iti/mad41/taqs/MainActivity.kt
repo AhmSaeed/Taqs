42af94b2c813feb7dd90213b14cdf5ac308c99b9
AhmSaeed <ahmedsaeedm1234@gmail.com>
2021-03-14 21:42:26 +0200

    Add home screen basic UI

--- app/src/main/java/com/iti/mad41/taqs/MainActivity.kt
+++ app/src/main/java/com/iti/mad41/taqs/MainActivity.kt
@@ -2,10 +2,20 @@ package com.iti.mad41.taqs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.iti.mad41.taqs.home.Home
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<Home>(R.id.fragment_container_view)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/iti/mad41/taqs/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.iti.mad41.taqs.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
